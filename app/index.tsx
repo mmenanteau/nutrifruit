2572dbd93f632134d7de2a1169c9e25fc5dc2aca
Mégane Menanteau <megane.menanteau@gmail.com>
2023-11-09 17:53:36 +0100

    Display sorted fruits list

--- app/index.tsx
+++ app/index.tsx
@@ -1,19 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { FlatList, StyleSheet, Text, View } from 'react-native';
 import { Fruit } from '@/types/Fruit';
 import { fetchFruits } from '@/api/fruits';
 
 export default function App() {
   const [fruits, setFruits] = useState<Fruit[]>([]);
 
+  const sortFruitsByName = (fruits: Fruit[]): Fruit[] => {
+    return fruits.sort((a, b) => a.name.localeCompare(b.name.toString()));
+  };
+
   useEffect(() => {
-    fetchFruits().then(setFruits);
+    fetchFruits().then(sortFruitsByName).then(setFruits);
   }, []);
 
   return (
     <View style={styles.container}>
       <Text>{fruits.length} fruits</Text>
+      <FlatList data={fruits} renderItem={({ item }) => {
+        return <Text>{item.name}</Text>
+      }} />
       <StatusBar style="auto" />
     </View>
   );
@@ -23,7 +30,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
